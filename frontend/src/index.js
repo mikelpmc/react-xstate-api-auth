99ea609ae0d741aa1d07e6fca6d88651a5d171a2
mikel <mikelpmc@gmail.com>
2020-04-01 18:10:11 +0200

    remove serviceWorker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,17 +6,13 @@ import 'normalize.css';
 import './index.css';
 
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-
 import AuthProvider from './provider/';
 
 ReactDOM.render(
-    <AuthProvider>
-        <Router>
-            <App />
-        </Router>
-    </AuthProvider>,
-    document.getElementById('root')
+  <AuthProvider>
+    <Router>
+      <App />
+    </Router>
+  </AuthProvider>,
+  document.getElementById('root')
 );
-
-registerServiceWorker();
